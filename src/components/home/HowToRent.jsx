976172097838bd44f003033ba2a411f37d70a135
Rahulraj888 <reddavenirahul@gmail.com>
2025-03-19 20:54:02 -0400

    home refactoring completed

--- src/components/home/HowToRent.jsx
+++ src/components/home/HowToRent.jsx
@@ -1,55 +1,16 @@
 import React from "react";
 import "../../styles/home.css";
+import HowToRentCard from "./HowToRentCard";
 
 export default function HowToRent() {
   return (
     <div className="container-fluid text-center my-4 py-4">
       <h2 className="text-body-emphasis">How to Rent?</h2>
       <div className="row my-4">
-        <div className="col-lg-3">
-          <figure className="elementor-image-box-img mb-3">
-            <img
-              width="140"
-              height="140"
-              src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/1-Select-product.png"
-              alt="Select Product"
-            />
-          </figure>
-          <p className="fw-normal">Select a Product</p>
-        </div>
-        <div className="col-lg-3">
-          <figure className="elementor-image-box-img mb-3">
-            <img
-              width="140"
-              height="140"
-              src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/2-Easy-KYC.png"
-              alt="Easy KYC"
-            />
-          </figure>
-          <p className="fw-normal">Easy KYC</p>
-        </div>
-        <div className="col-lg-3">
-          <figure className="elementor-image-box-img mb-3">
-            <img
-              width="140"
-              height="140"
-              src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/3-Pay-once-satisfied.png"
-              alt="Pay once satisfied"
-            />
-          </figure>
-          <p className="fw-normal">Pay Once Satisfied</p>
-        </div>
-        <div className="col-lg-3">
-          <figure className="elementor-image-box-img mb-3">
-            <img
-              width="140"
-              height="140"
-              src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/4-Product-Delivered.png"
-              alt="Product Delivered"
-            />
-          </figure>
-          <p className="fw-normal">Product Delivered</p>
-        </div>
+        <HowToRentCard src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/1-Select-product.png" title="Select Product"/>
+        <HowToRentCard src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/2-Easy-KYC.png" title="Easy KYC"/>
+        <HowToRentCard src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/3-Pay-once-satisfied.png" title="Pay once satisfied"/>
+        <HowToRentCard src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/4-Product-Delivered.png" title="Product Delivered"/>
       </div>
     </div>
   );
--- src/components/home/WhyToRent.jsx
+++ src/components/home/WhyToRent.jsx
@@ -1,55 +1,28 @@
 import React from "react";
 import "../../styles/home.css";
+import WhyToRentCard from "./WhyToRentCard";
 
 export default function WhyToRent() {
   return (
     <div className="container-fluid text-center my-4 py-4 bg-body-tertiary border-top border-bottom">
       <h2 className="text-body-emphasis">Why to Rent Out?</h2>
       <div className="row my-4">
-        <div className="col-lg-3">
-          <figure className="elementor-image-box-img mb-3">
-            <img
-              width="70"
-              height="70"
-              src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/Best-Competitive-Price.png"
-              alt="Best Competitive Price"
-            />
-          </figure>
-          <p className="fw-normal">Best Competitive Price</p>
-        </div>
-        <div className="col-lg-3">
-          <figure className="elementor-image-box-img mb-3">
-            <img
-              width="70"
-              height="70"
-              src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/Flexible-Update.png"
-              alt="Flexible Update"
-            />
-          </figure>
-          <p className="fw-normal">Flexible Update</p>
-        </div>
-        <div className="col-lg-3">
-          <figure className="elementor-image-box-img mb-3">
-            <img
-              width="70"
-              height="70"
-              src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/Quality-Services.png"
-              alt="Quality Services"
-            />
-          </figure>
-          <p className="fw-normal">Quality Services</p>
-        </div>
-        <div className="col-lg-3">
-          <figure className="elementor-image-box-img mb-3">
-            <img
-              width="70"
-              height="70"
-              src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/On-Time-Availability.png"
-              alt="On-Time Availability"
-            />
-          </figure>
-          <p className="fw-normal">On-Time Availability</p>
-        </div>
+        <WhyToRentCard
+          src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/Best-Competitive-Price.png"
+          title="Best Competitive Price"
+        />
+        <WhyToRentCard
+          src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/Flexible-Update.png"
+          title="Flexible Update"
+        />
+        <WhyToRentCard
+          src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/Quality-Services.png"
+          title="Quality Services"
+        />
+        <WhyToRentCard
+          src="https://www.vrsitrentals.com/shop/wp-content/uploads/2024/02/On-Time-Availability.png"
+          title="On-Time Availability"
+        />
       </div>
     </div>
   );
